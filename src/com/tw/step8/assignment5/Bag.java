ff6e0c369d7a274652973b64990a42c6e84951c7
Roshan P R <99485730+roshan-pr@users.noreply.github.com>
2022-09-19 12:22:50 +0530

    prevented having more than double the number of red balls as there are green balls

--- src/com/tw/step8/assignment5/Bag.java
+++ src/com/tw/step8/assignment5/Bag.java
@@ -2,6 +2,9 @@ package com.tw.step8.assignment5;
 
 import java.util.HashSet;
 
+// As a wizard I’d like a bag that prevents me from having more than double the number of red balls as there are green balls
+// given ball is red ?
+
 public class Bag {
   private final HashSet<Ball> balls;
   private final int capacity = 12;
@@ -10,17 +13,41 @@ public class Bag {
     this.balls = new HashSet<>(this.capacity);
   }
 
-  public static Bag createStore() {
+  public static Bag create() {
     return new Bag();
   }
 
   public boolean add(Ball ball) {
     if (isBagFull()) return false;
 
-    if (isReachedMaxLimit(Color.GREEN, 3)) {
+    int greenBallsLimit = 3;
+    if (isReachedMaxLimit(Color.GREEN, greenBallsLimit)) {
       return false;
     }
+
+    if (ball.getColor() == Color.RED && !isRedAllowed()) {
+      return false;
+    }
+
     return this.balls.add(ball);
+  }
+
+  private boolean isRedAllowed() {
+    if (isBagEmpty()) {
+      return false;
+    }
+    long ratio = countOf(Color.RED) / countOf(Color.GREEN);
+    return ratio < 2.0;
+  }
+
+  private boolean isBagEmpty() {
+    return this.balls.size() == 0;
+  }
+
+  private long countOf(Color color) {
+    return this.balls.stream()
+            .filter(ball -> ball.getColor() == color)
+            .count();
   }
 
   private boolean isBagFull() {
@@ -28,9 +55,6 @@ public class Bag {
   }
 
   private boolean isReachedMaxLimit(Color color, int maxLimit) {
-    long count = this.balls.stream()
-            .filter(ball -> ball.getColor() == color)
-            .count();
-    return count >= maxLimit;
+    return countOf(color) >= maxLimit;
   }
 }
--- test/com/tw/step8/assignment5/BagTest.java
+++ test/com/tw/step8/assignment5/BagTest.java
@@ -7,16 +7,56 @@ import static org.junit.jupiter.api.Assertions.*;
 class BagTest {
   @Test
   void shouldAddABall() {
-    Bag bag = Bag.createStore();
+    Bag bag = Bag.create();
+
     assertTrue(bag.add(new Ball("ball-1", Color.GREEN)));
   }
 
   @Test
+  void shouldNotAddMoreThanTwelveBalls() {
+    Bag bag = Bag.create();
+
+    for (int i = 0; i < 12; i++) {
+      bag.add(new Ball("ball-" + i, Color.NOCOLOR));
+    }
+
+    assertFalse(bag.add(new Ball("ball-13", Color.GREEN)));
+  }
+
+  @Test
   void shouldNotAddMoreThanThreeGreenBalls() {
-    Bag bag = Bag.createStore();
+    Bag bag = Bag.create();
+
     bag.add(new Ball("ball-1", Color.GREEN));
     bag.add(new Ball("ball-1", Color.GREEN));
     bag.add(new Ball("ball-1", Color.GREEN));
+
     assertFalse(bag.add(new Ball("ball-1", Color.GREEN)));
+  }
+
+  @Test
+  void shouldPreventAddingRedBallIfNoGreenBalls() {
+    Bag bag = Bag.create();
+
+    assertFalse(bag.add(new Ball("ball-1", Color.RED)));
+  }
+
+  @Test
+  void shouldAddTwoRedBallForOneGreenBall() {
+    Bag bag = Bag.create();
+
+    bag.add(new Ball("ball-1", Color.GREEN));
+    bag.add(new Ball("ball-3", Color.RED));
+    assertTrue(bag.add(new Ball("ball-4", Color.RED)));
+  }
+
+  @Test
+  void shouldPreventAddingThreeRedBallForTwoGreenBalls() {
+    Bag bag = Bag.create();
+
+    bag.add(new Ball("ball-1", Color.GREEN));
+    bag.add(new Ball("ball-2", Color.RED));
+    bag.add(new Ball("ball-3", Color.RED));
+    assertFalse(bag.add(new Ball("ball-4", Color.RED)));
   }
 }
